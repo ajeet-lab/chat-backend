38af5394ce0c57d084aa5a99ffb8e3b770defa94
AJEET KUSHWAHA <51816732+ajeet-lab@users.noreply.github.com>
2023-03-03 17:26:20 +0530

    changes

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ const server = app.listen(
 const io = require("socket.io")(server, { 
   pingTimeout: 60000,
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://ajeet-chat.netlify.app/",
     // credentials: true,
   },
 });
